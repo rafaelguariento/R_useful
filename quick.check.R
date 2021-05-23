f8a774216d34b0201673b3febf2d41102825e476
rafaelguariento <84686884+rafaelguariento@users.noreply.github.com>
2021-05-23 19:02:20 -0300

    Add files via upload

---
+++ quick.check.R
@@ -0,0 +1,24 @@
+quick.check <- function(mod) # quick check concerning fixed effects in lmer
+  
+{            
+  par(mfrow=c(1,2), mar= c(5,4,2,2))
+  
+  plot(resid(mod) ~ fitted(mod), xlab = "Fitted values", ylab ="Residuals")
+  
+  abline(h=0)
+  
+  abline(lm(resid(mod)~fitted(mod)), col= "red")        
+  
+  qqnorm(resid(mod), ylab="Residuals")
+  
+  qqline(resid(mod))
+  
+  plot(density(resid(mod)))
+  
+  rug(resid(mod))
+  
+  qqnorm(fitted(mod), main = "QQ plot fitted")
+  
+  qqline(fitted(mod))
+  
+  print(anova(lm(fitted(mod)~resid(mod))))}
